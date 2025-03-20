686973c352404178b17b0dba27f9a57979725b76
Ayham Al-Nammourah <158759366+AyhamAlnammourah@users.noreply.github.com>
2025-03-20 05:20:06 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -31,3 +31,52 @@ window.addEventListener("scroll", () => {
     header.classList.remove("header--scroll");
   }
 });
+
+// ========================
+
+let features_Resourses = [
+  {
+    title: "bookmark in one click",
+    paragraph: `organize your bookmarks however you like. our simple
+                drag-and-drop interface gives you complete control over how you
+                manage favourite sites.`,
+    image: "images/illustration-features-tab-1.svg",
+    tap: "tap1",
+  },
+  {
+    title: "intelligent search",
+    paragraph: `our powerful search feature will help you find saved sites in no
+                time at all. no need to trawl through all of your bookmarks.`,
+    image: "images/illustration-features-tab-2.svg",
+    tap: "tap2",
+  },
+  {
+    title: "share your bookmarks",
+    paragraph: `easily share your bookmarks and collections with others. create
+                a shareable link that you can send at the click of a button.`,
+    image: "images/illustration-features-tab-3.svg",
+    tap: "tap3",
+  },
+];
+
+// DOM
+let features_title = document.getElementById("features-title");
+let features_paragraph = document.getElementById("features-paragraph");
+let features_image = document.getElementById("features-image");
+let features_buttons_contant = document.querySelectorAll(
+  ".features-buttons-contant"
+);
+
+function featureFun(tapContant) {
+  features_buttons_contant.forEach((btn) => {
+    btn.classList.remove("active");
+  });
+  for (let i = 0; i < features_Resourses.length; i++) {
+    if (tapContant == features_Resourses[i].tap) {
+      features_title.textContent = features_Resourses[i].title;
+      features_paragraph.textContent = features_Resourses[i].paragraph;
+      features_image.setAttribute("src", features_Resourses[i].image);
+      features_buttons_contant[i].classList.add("active");
+    }
+  }
+}
